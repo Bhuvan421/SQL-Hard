8af0e6dabc4424d395846707a3955deed4141c49
Bhuvanendra Jonnalagadda <bhuvan102600@gmail.com>
2024-11-06 00:18:28 -0600

    Create stock-min-max-part1.sql

---
+++ SQL_Medium_Hard/stock-min-max-part1.sql
@@ -0,0 +1,44 @@
+## Question: 
+  https://datalemur.com/questions/sql-bloomberg-stock-min-max-1
+
+## Solution:
+
+
+with highestOpen as 
+(
+  select 
+    ticker, 
+    to_char(date, 'Mon-YYYY') as highest_mth, 
+    max(open) as highest_open, 
+    row_number() over(partition by ticker order by max(open) desc) as high_rank
+  from 
+    stock_prices 
+  group by 
+    ticker, date
+), 
+lowestOpen as 
+(
+  select 
+    ticker, 
+    to_char(date, 'Mon-YYYY') as lowest_mth, 
+    min(open) as lowest_open, 
+    row_number() over(partition by ticker order by min(open)) as low_rank 
+  from 
+    stock_prices 
+  group by 
+    ticker, date
+)
+
+select 
+  ho.ticker, 
+  highest_mth, 
+  highest_open, 
+  lowest_mth, 
+  lowest_open 
+from 
+  highestOpen ho inner join lowestOpen lo 
+  on ho.ticker = lo.ticker 
+where 
+  ho.high_rank = 1 and lo.low_rank = 1
+order by 
+  ho.ticker

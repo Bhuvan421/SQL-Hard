932c8069ed523ad472da46a6b0c960dc52aae102
Bhuvanendra Jonnalagadda <bhuvan102600@gmail.com>
2024-11-06 12:11:56 -0600

    Create Patient-Support-Analysis-Part-2.sql

---
+++ SQL_Medium_Hard/Patient-Support-Analysis-Part-2.sql
@@ -0,0 +1,23 @@
+### Question: 
+  https://datalemur.com/questions/uncategorized-calls-percentage
+
+  
+### Solution:
+
+-- SELECT count(*) FROM callers;
+
+-- select distinct call_category from callers
+
+-- select count(*) from callers where call_category = 'n/a' or call_category is null 
+
+with totalCount as 
+(
+  SELECT count(*) as t FROM callers
+),
+uncatCount as 
+(
+  select count(*) as ut from callers where call_category = 'n/a' or call_category is null 
+)
+
+select 
+  round(((select ut::numeric from uncatCount) / (select t from totalCount)) * 100, 1) as uncategorized_call_pct

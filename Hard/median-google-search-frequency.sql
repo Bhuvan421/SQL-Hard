0a3d57d319dd04e2dd8be7b534a9e101854c371e
Bhuvanendra Jonnalagadda <bhuvan102600@gmail.com>
2024-11-07 13:54:02 -0600

    Update median-google-search-frequency.sql

--- Hard/median-google-search-frequency.sql
+++ Hard/median-google-search-frequency.sql
@@ -40,6 +40,46 @@ SELECT
       where user_order = (max_order + min_order) / 2.0)
   end as median 
 from 
+  order_stats;
+
+
+
+--> #### You can use cross-join as well -> straightforward
+
+
+with user_distribution as 
+(
+  SELECT 
+    searches, 
+    row_number() over(order by searches) as user_order
+  FROM 
+    search_frequency
+  cross join
+    GENERATE_SERIES(1, num_users) as series
+), 
+order_stats as 
+(
+select 
+  max(user_order) as max_order, 
+  min(user_order) as min_order
+from 
+  user_distribution
+)
+SELECT
+  case 
+    when max_order % 2 = 0 then 
+    (select round(avg(searches), 1) 
+      from user_distribution 
+      where user_order = floor((max_order + min_order) / 2.0) 
+      or user_order = ceil((max_order + min_order) / 2.0))
+    else 
+    (select searches 
+      from user_distribution 
+      where user_order = (max_order + min_order) / 2.0)
+  end as median 
+from 
   order_stats
+
+
 
 
